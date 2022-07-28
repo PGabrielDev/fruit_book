9164c987d046a7e9350746f854d779f4442df677
Paulo G G Matias <pgabrieldeveloper@outlook.com>
2022-07-28 13:17:31 -0300

    criando configurações do banco de dados

---
+++ app/core/db.py
@@ -0,0 +1,9 @@
+from click import echo
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
+
+from core.configs import settings
+
+engine: AsyncEngine = create_async_engine(settings.DB_URL)
+
+Session: AsyncSession = sessionmaker(autocommit=False, autoflush=False, expire_on_commit=False, echo=True class_=AsyncSession, bind=engine)

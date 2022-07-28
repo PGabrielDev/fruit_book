82384e6ce2d915743fd43e83edecba42e4b0ce55
Paulo G G Matias <pgabrieldeveloper@outlook.com>
2022-07-28 13:59:55 -0300

    criando model de vendas

---
+++ app/models/__all_models.py
@@ -0,0 +1,1 @@
+from models.vendas import Venda
---
+++ app/models/cliente.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from email.policy import default
+from core.configs import settings
+from sqlalchemy import Column, Integer, String, DateTime,DECIMAL
+
+
+class Venda(settings.DB_BASE_MODEL):
+    __tablename__ = 'tb_venda'
+
+    id: int =  Column(Integer, primary_key=True, autoincrement=True)
+    titulo: str =  Column(String(100), nullable=False)
+    descricao: str =  Column(String)
+    valor: float =  Column(DECIMAL(8,2),nullable=False)
+    data_venda: datetime = Column(DateTime, default=datetime.now)
+    data_pagamento: datetime = Column(DateTime)

53f97e8f6312df6ba66e7a9ad8a9d02ad51c0104
Paulo G G Matias <pgabrieldeveloper@outlook.com>
2022-07-29 17:17:38 -0300

    ajustanto endpoints

--- app/routes/router_venda.py
+++ app/routes/router_venda.py
@@ -16,7 +16,7 @@ from core.deps import get_session
 router = APIRouter()
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=VendaSchema)
-async def salvar_fruit_venda(venda: VendaSchema, db: AsyncSession =  Depends(get_session)):
+async def salvar_fruit_venda(venda: VendaSchema, db: AsyncSession = Depends(get_session)):
     fruit_venda = Venda(
         titulo=venda.titulo,
         descricao=venda.descricao,
@@ -39,11 +39,28 @@ async def get_vendas(db: AsyncSession = Depends(get_session)):
 @router.get('/{id}', response_model=VendaSchema)
 async def get_venda(id:int, db: AsyncSession = Depends(get_session)):
     async with db as session:
-        query = session(Venda).filter(Venda.id == id)
+        query = select(Venda).filter(Venda.id == id)
         resultado = await session.execute(query)
         venda = resultado.scalar_one_or_none()
-
         if venda:
             return venda
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Venda nao encontrada !")
+
+@router.put('/{id}', response_model=VendaSchema)
+async def put_venda(id:int, venda: VendaSchema, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(Venda).filter(Venda.id == id)
+        resultado = await session.execute(query)
+        venda_db: Venda = resultado.scalars().first()
+        if venda_db:
+            venda_db.titulo = venda.titulo
+            venda_db.descricao = venda.descricao
+            venda_db.valor = venda.valor
+            venda_db.data_prevista_pagamento = venda.data_prevista_pagamento
+            venda_db.data_pagamento = venda.data_pagamento
+            venda_db.data_prevista_pagamento = venda.data_prevista_pagamento
+            await session.commit()
+            return venda_db
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Venda nao encontrada !")

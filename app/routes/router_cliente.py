7fa2f139b224a34e82db3a93e4113041e31c4df0
Paulo G G Matias <pgabrieldeveloper@outlook.com>
2022-07-30 10:31:58 -0300

    corrindo erros router_client

--- app/routes/router_cliente.py
+++ app/routes/router_cliente.py
@@ -1,5 +1,5 @@
-from re import I
 from typing import List
+
 
 from fastapi import APIRouter
 from fastapi import status
@@ -35,8 +35,11 @@ async def get_client_by_id(id: int, db: AsyncSession = Depends(get_session)):
     async with db as session:
         query = select(Cliente).filter(Cliente.id == id)
         resultado = await session.execute(query)
-        cliente = resultado.scalars().unique().first()
-        return cliente
+        cliente = resultado.unique().scalar_one_or_none()
+        if cliente:
+            return cliente
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cliente não encontrado !")
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_client(name: str = Query(default=None, description="Para buscar todos os clientes com o nome escrito", title="Nome"), db: AsyncSession = Depends(get_session)):
@@ -47,32 +50,39 @@ async def get_client(name: str = Query(default=None, description="Para buscar to
             query = select(Cliente).filter(Cliente.nome.like(f'%{name}%'))
         resultado = await session.execute(query)
         clientes : List[Cliente] = resultado.scalars().unique().all()
+        for cliente in clientes:
+            del cliente.vendas
         return clientes
 
-@router.post('/compra/{id}')
+
+@router.post('/compra/{id}', response_model=List[VendaSchema])
 async def post_cliente_compra(id: int, compras: List[VendaSchema], db: AsyncSession = Depends(get_session)):
+
     async with db as session:
         novas_compras = []
         query = select(Cliente).filter(Cliente.id == id)
         resultado = await session.execute(query)
-        cliente: Cliente = resultado.scalars().unique().first()
+        cliente: Cliente = resultado.unique().scalar_one_or_none()
         if cliente:
             for compra in compras:
-                novas_compras.append( Venda(
-                titulo=compra.titulo,
-                descricao=compra.descricao,
-                valor=compra.valor,
-                data_prevista_pagamento=compra.data_prevista_pagamento
-        ))
+                nova_compra = await salvar_venda(criar_venda(compra=compra), session)
+                novas_compras.append(nova_compra)
             cliente.vendas.extend(novas_compras)
             await session.commit()
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cliente não encontrado !")   
-        
-        await session.commit()
         return compras
 
 
+async def salvar_venda(compra: Venda, session: AsyncSession):
+    session.add(compra)
+    await session.commit()
+    return compra
 
 
-     
+def criar_venda(compra: VendaSchema):
+    return Venda(
+                titulo=compra.titulo,
+                descricao=compra.descricao,
+                valor=compra.valor,
+                data_prevista_pagamento=compra.data_prevista_pagamento)
